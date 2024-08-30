6c3f93974a869bd5ef1d1af0bdf15afac17c1819
Frances Price-Herndl <fp22b@fsu.edu>
2024-08-30 16:46:38 -0400

    finished tutorial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  // Creates a canvas 600 pixels wide
+  // and 400 pixels high.
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  //creates a skyblue background
+  background(135,206,235);
+  //sun in top right
+  fill("yellow"); //yellow
+  stroke("orange"); //orange outline
+  strokeWeight(20); // large outline
+  circle(550,50,100);
+
+  stroke(0);
+  strokeWeight(1);
+
+  //grass
+  fill("green");
+  rect(0,200,600,200);
+
+  //emojis
+  textSize(75);
+  text("🌸",100,250); //sheep
+  text("🐞", mouseX,mouseY); //ladybug
 }
